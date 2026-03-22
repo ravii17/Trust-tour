b9ea5b60350af40b9075fa014dde3ae7cd1e3871
ravii17 <ravikumar75890@gmail.com>
2026-03-23 00:14:57 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-const isDev = process.env.NODE_ENV === "development";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    isDev && componentTagger()
-  ].filter(Boolean),
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
